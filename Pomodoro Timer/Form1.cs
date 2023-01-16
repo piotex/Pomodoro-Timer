cfc672808a93e6e2545f700d606273102d57336d
piotex <pkubon2@gmail.com>
2023-01-16 11:55:25 +0100

    count down timer

--- Pomodoro Timer/Form1.cs
+++ Pomodoro Timer/Form1.cs
@@ -7,49 +7,85 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Pomodoro_Timer
 {
     public partial class Form1 : Form
     {
+        PomodoroProgressBar bar;
+        private static DateTime endTime;
+        System.Timers.Timer myTimer;
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        PomodoroProgressBar bar;
         private void Form1_Load(object sender, EventArgs e)
         {
             bar = new PomodoroProgressBar();
-            bar.Location = new System.Drawing.Point(24, 230);
-            bar.Size = new System.Drawing.Size(385, 20);
+            bar.Location = new System.Drawing.Point(24, 175);
+            bar.Size = new System.Drawing.Size(275, 20);
             bar.TabIndex = 1;
             bar.Value = 30;
             main_panel.Controls.Add(bar);
         }
 
-        private void timer_label_Click(object sender, EventArgs e)
+        private void button_studyTime_Click(object sender, EventArgs e)
         {
+            myTimer = new System.Timers.Timer();
+            myTimer.Elapsed += new ElapsedEventHandler(decrease_timer_val);
+            myTimer.Interval = 1300; // 1000 ms is one second
+            myTimer.Start();
 
+            int min_to_work = int.Parse(textBox_studyTime.Text);
+            endTime = DateTime.Now.AddMinutes(min_to_work);
+            bar.Value= 0;
+            bar.Maximum = min_to_work * 60;
+        }
+        private void button_breakTime_Click(object sender, EventArgs e)
+        {
+            myTimer = new System.Timers.Timer();
+            myTimer.Elapsed += new ElapsedEventHandler(decrease_timer_val);
+            myTimer.Interval = 1300; // 1000 ms is one second
+            myTimer.Start();
+
+            int min_to_work = int.Parse(textBox_breakTime.Text);
+            endTime = DateTime.Now.AddMinutes(min_to_work);
+            bar.Value = 0;
+            bar.Maximum = min_to_work * 60;
+        }
+
+        public void decrease_timer_val(object source, ElapsedEventArgs e)
+        {
+            TimeSpan tmpTime = endTime - DateTime.Now;
+
+            if (tmpTime.TotalSeconds < 0)
+            {
+                myTimer.Stop();
+                WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+                wplayer.URL = @"C:\Users\pkubo\Desktop\Pomodoro-Timer\Pomodoro Timer\Desk-bell-small-sound-effect.mp3";
+                wplayer.controls.play();
+            }
+
+            if (this.timer_label.InvokeRequired)
+            {
+                this.timer_label.BeginInvoke((MethodInvoker)delegate () {
+                    timer_label.Text = tmpTime.ToString("mm':'ss");
+                });
+                this.bar.BeginInvoke((MethodInvoker)delegate () {
+                    bar.Value += 1;
+                });
+            }
         }
 
 
-        private int counter = 0;
-        private void button1_Click(object sender, EventArgs e)
+        private void timer_label_Click(object sender, EventArgs e)
         {
-            counter ++;
-            int mod_max = 5;
-            if (counter % mod_max == 0)
-                bar.Value = 0;
-            if (counter % mod_max == 1)
-                bar.Value = 30;
-            if (counter % mod_max == 2)
-                bar.Value = 50;
-            if (counter % mod_max == 3)
-                bar.Value = 70;
-            if (counter % mod_max == 4)
-                bar.Value = 100;
+
         }
     }
 }
